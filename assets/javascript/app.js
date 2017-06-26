09c315c0f51336c6235bcce88073176cfecb7c84
MB DR <mderoberts@users.noreply.github.com>
2017-06-25 23:41:09 -0400

    add DOM elements dynamically, play gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-
-var characters = ["Michael Bluth", "George Bluth", "Gob Bluth", "Lucielle Bluth", "Tobias Funke"];
-
+// Define array
+var characters = ["George Bluth", "Gob Bluth", "Tobias Funke"];
+// Load buttons from array
 function loadBtns(){
     for (var i = 0; i < characters.length; i++) {
         makeBtn(characters[i]);
     }
 }
-
 loadBtns();
-
+// Button maker
 function makeBtn(character) {
     var button = $("<button>");
     button.addClass("character");
@@ -17,9 +16,9 @@ function makeBtn(character) {
     button.text(character);
     $("#buttons").append(button);
 }
-
+// Button event handler, assign API endpoint
 $(".character").on("click", function() {
-    var person = $(this).attr("data-name");
+    var person = $(this).text();
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
     person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -31,11 +30,17 @@ $(".character").on("click", function() {
     .done(function(response) {
         console.log(queryURL);
         console.log(response);
-
+        // Take response data and add new elements to DOM
         var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            var characterDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var characterImage = $("<img>");
+            characterImage.attr("src", results[i].images.fixed_height.url);
+            characterDiv.append(p);
+            characterDiv.append(characterImage);
+            $("#gifs").prepend(characterDiv);
+        }
     })
 })
-
-
-
 });
