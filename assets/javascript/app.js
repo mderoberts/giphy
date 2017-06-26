c71b50d081a8424aa066b08efe807f4efbc9f7b3
MB DR <mderoberts@users.noreply.github.com>
2017-06-25 20:19:24 -0400

    button function with console log

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
 
 var characters = ["Michael Bluth", "George Bluth", "Gob Bluth", "Lucielle Bluth", "Tobias Funke"];
 
-for (var i = 0; i < characters.length; i++) {
-
+function loadBtns(){
+    for (var i = 0; i < characters.length; i++) {
+        makeBtn(characters[i]);
+    }
 }
 
-function makeBtn() {
+loadBtns();
+
+function makeBtn(character) {
     var button = $("<button>");
+    button.addClass("character");
+    button.attr("data-name",character);
+    button.text(character);
     $("#buttons").append(button);
 }
 
-$(".charBtn").on("click", function() {
-    var person = $(this).attr("data-person");
+$(".character").on("click", function() {
+    var person = $(this).attr("data-name");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
     person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
